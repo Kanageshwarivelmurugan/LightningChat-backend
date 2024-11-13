5d7e702318572c50c62402fddb031ea9cc3ceb76
Kanageshwarivelmurugan <vkanageshwari73@gmail.com>
2024-11-13 17:44:03 +0530

    thirdcommit

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.ona8z.mongodb.net/Chatapp?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(`mongodb+srv://vkanageshwari73:Mongo18@cluster0.ona8z.mongodb.net/Chatapp?retryWrites=true&w=majority&appName=Cluster0`)
 .then(() => {
     console.log('Connected to MongoDB');
   })
