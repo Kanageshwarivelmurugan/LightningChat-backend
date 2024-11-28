9c342dbbec02612e8318b3e4119ad8aaac601075
Kanageshwarivelmurugan <vkanageshwari73@gmail.com>
2024-11-28 17:35:36 +0530

    urlchange

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 //const io = socketIo(server);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173/',
+    origin: '	https://lightning-chat-frontend-git-master-kanageshwaris-projects.vercel.app/',
     methods: ['GET', 'POST']
   }
 })
