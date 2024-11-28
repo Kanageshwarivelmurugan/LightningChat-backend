24283700d04f104920eb91a64ffcf3a71947395c
Kanageshwarivelmurugan <vkanageshwari73@gmail.com>
2024-11-28 22:39:30 +0530

    fileshare

--- models/Message.js
+++ models/Message.js
@@ -6,9 +6,10 @@ const MessageSchema = new mongoose.Schema({
   socketid: String,
   time: String,
   date: String,
-  to: String
-})
+  to: String,
+  fileUrl: String  // New field to store the file URL
+});
 
 const Message = mongoose.model('Message', MessageSchema);
 
-module.exports = Message
+module.exports = Message;
